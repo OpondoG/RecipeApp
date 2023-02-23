77a83c29625a39da1351e7d05d7211d7060d4626
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-02-23 19:32:28 +0100

    Add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "foods#index"
+   devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
+  root "users#index"
   resources :users, only: [:index, :show] do
     resources :foods, only: [:index, :show, :new, :create, :destroy]
   end
