aff7d6bb366ad36a7c32bcbf34b7ee5e8275fc2d
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-02-24 20:51:56 +0100

    Add public routes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,18 @@
 class Recipe < ApplicationRecord
-  belongs_to :user
+   belongs_to :user
   has_many :recipe_foods
 
   validates :name, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
   validates :description, presence: true
+  validates :public, presence: true
+
+  def total_price
+    recipe_foods.sum { |item| item.quantity * item.food.price }
+  end
+
+  def total_quantity
+    recipe_foods.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-
-   devise_for :users, controllers: {
+  devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
-
   root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :foods, only: [:index, :show, :new, :create, :destroy]
+  resources :users, only: [:index] do
+    resources :foods, only: [:index, :new, :create, :destroy]
+    resources :public_recipes, only: [:index]
     resources :recipes, only: [:index, :new, :show, :create, :destroy] do
       resources :recipe_foods, only: [:new, :edit, :create, :update, :destroy]
-      resources :general_shopping_list, only: [:index]
+      resources :shopping_lists, only: [:index]
     end
   end
 end
