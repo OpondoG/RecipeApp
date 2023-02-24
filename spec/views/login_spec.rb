c97cb3066978a28ddd8959f99e0b4266c79012bf
Orji Maxwell <chukwuemekaorji678@gmail.com>
2023-02-24 22:45:27 +0100

    Add test

---
+++ spec/views/login_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'Log In', type: :feature do
+  before :each do
+    @user = User.create(name: 'John Doe', email: 'john@email.com', password: '123456', confirmed_at: Time.now)
+    visit new_user_session_path
+  end
+
+  it 'should not sign me in when the credentials are not correct' do
+    within('#new_user') do
+      fill_in 'user_email', with: 'noemail@email.com'
+      fill_in 'user_password', with: '123456'
+    end
+    click_button 'Log in'
+    expect(current_path).to match new_user_session_path
+    expect(page).to have_content 'Invalid Email or password'
+  end
+end
